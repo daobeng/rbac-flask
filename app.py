24aacc52152a276ce8aa3ec735e1172047a8f974
daobeng <daniel.obeng@givewith.com>
2021-06-07 14:13:35 -0400

    adds users

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request
+from users import users
+
+def authenticate_request():
+    user = request.headers.get('authorization', '')
+    request.user = users[user.split(' ')[-1]]
 
 blueprint = Blueprint('test', __name__)
 
 # ------ ROUTES -----------------
 @blueprint.route('/', methods=['GET'])
 def hello_world():
-    return 'Get Hello, World!'
+    return f'GET: Welcome { request.user["username"] }'
 
 @blueprint.route('/', methods=['POST'])
 def post_hello_world():
-    return 'Post hello world!'
+    return f'POST: You posted { request.user["username"] }'
 # --------------------------------------
 
+
 app = Flask(__name__)
+blueprint.before_request(authenticate_request)
 app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
---
+++ users.py
@@ -0,0 +1,18 @@
+users = {
+    'basicUser': {
+        'username': 'Jill',
+        'roles': [],
+    },
+    'ITUser': {
+        'username': 'Mary',
+        'roles': ['IT'],
+    },
+    'ITAndMarketing': {
+        'username': 'Jack',
+        'roles': ['marketing', 'IT'],
+    },
+    'superUser': {
+        'username': 'Jake',
+        'roles': ['superuser'],
+    }
+}
