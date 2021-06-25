3aee8882e70dabfde1c9717a0aae41e4dae1d526
daobeng <daniel.obeng@givewith.com>
2021-06-25 16:19:46 -0400

    modifies for custom support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, request, g, jsonify, url_for
 import casbin
 from adapter import ArrayAdapter
+from functools import wraps
 from users import users
 
 blueprint = Blueprint('test', __name__)
@@ -16,15 +17,16 @@ app = Flask(__name__)
 rules = [
     # group/policy associations
     # (p - policy, subject, object - url endpoint, action)
-    ('p', 'IT', '/', 'GET'),
-    ('p', 'marketing', '/', '*'),
-    ('p', 'marketing', '/*', 'GET'),
-    ('p', 'marketing', '/:id/test', '*'),
+    ('p', '','IT', '/', 'GET'),
+    ('p', '', 'marketing', '/', '*'),
+    ('p', '', 'marketing', '/*', 'GET'),
+    ('p', '', 'marketing', '/:id/test', '*'),
+    ('p', 'r.sub == r.obj.owner', '*', 'deal', 'delete')
 
     # group associations
     # (g - group, subject, subject) - a superuser is an IT user as well as marketing user
-    ('g', 'superuser', 'IT'),
-    ('g', 'superuser', 'marketing'),
+    # ('g', 'superuser', 'IT'),
+    # ('g', 'superuser', 'marketing'),
 ]
 
 adapter = ArrayAdapter(rules)
@@ -36,41 +38,66 @@ def before_request():
     g.current_user = user
 
 # use casbin enforcer as middleware
-def casbin_enforcer_middleware():
-    # enforce permissions if X-EnforcePermissions: true is passed in header as roll out plan
-    use_enforcer = request.headers.get('X-EnforcePermissions', '').lower() == 'true'
+def enforcer(view_func):
 
-    if use_enforcer:
+    @wraps(view_func)
+    def enforce(*args, **kwargs):
         uri = str(request.path)
         for role in g.current_user['roles']:
-            if casbin_enforcer.enforce(role, uri, request.method):
-                break
+            if casbin_enforcer.enforce(role, None, uri, request.method):
+                return view_func(*args, **kwargs)
         else:
             user = g.current_user['username']
             app.logger.error(f'Unauthorized attempt: method: { request.method } resource: { uri } by { user }')
             return (jsonify({'message': 'Unauthorized'}), 401)
 
 
+    return enforce
+
+
+# enforce roles on special entities
+proposal = {
+    'name': 'my proposal',
+    'price': 25000,
+    'owner': 'Mary',
+    'casbinType': 'deal',
+}
+
 # ------ ROUTES -----------------
 @blueprint.route('/', methods=['GET'])
+@enforcer
 def hello_world():
     return f'GET: Welcome { g.current_user["username"] }'
 
 @blueprint.route('/', methods=['POST'])
+@enforcer
 def post_hello_world():
     return f'POST: You posted { g.current_user["username"] }'
 
 @blueprint.route('/', methods=['PATCH'])
+@enforcer
 def patch_hello_world():
     return f'POST: You patched { g.current_user["username"] }'
 
 @blueprint.route('/all', methods=['GET'])
+@enforcer
 def get_all_hello_world():
     return f'POST: You patched { g.current_user["username"] }'
 
 @blueprint.route('/<int:id>/test', methods=['GET'])
+@enforcer
 def final_one(id):
     return f'POST: { id }'
+
+@blueprint.route('/proposal', methods=['DELETE'])
+def get_my_proposal():
+    db_proposal = proposal
+
+    # can manually do a superuser check if that's relevant
+    if casbin_enforcer.enforce(g.current_user['username'], db_proposal, 'deal', 'delete'):
+        return f'Successfully deleted { proposal["name"] }'
+    else:
+        return (jsonify({'message': 'Unauthorized'}), 401)
 
 
 @blueprint.route('/site-map', methods=['GET'])
@@ -88,7 +115,7 @@ def site_map():
 
 
 app.before_request(before_request)
-app.before_request(casbin_enforcer_middleware)
+# app.before_request(casbin_enforcer_middleware)
 app.register_blueprint(blueprint)
 
 
